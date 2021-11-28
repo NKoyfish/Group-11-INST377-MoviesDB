16c922a82838420bf4bf4a301791b738aa4b1a3f
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-11-27 22:00:07 -0500

    search+ styling changes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,3 @@
-import DietaryRestrictions from './DietaryRestrictions.js';
-import DiningHall from './DiningHall.js';
-import Macros from './Macros.js';
-import Meals from './Meals.js';
 import Film from './film.js';
 import Actor from './actors.js';
 import Genre from './genres.js';
