b89ba5f50a41d162d0aa23c88a8c15f8e675d42a
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-12-03 19:00:25 -0500

    editform testing preairport
    more testing

--- client/public/editfilm.js
+++ client/public/editfilm.js
@@ -3,35 +3,81 @@ async function editFilmLoad() {
   const lookup = document.querySelector('#lookup');
   const id = document.querySelector('#id');
   const editform = document.querySelector('#editform');
-  const name = document.querySelector('#title');
-  const genre_id = document.querySelector('#genre');
-  const score = document.querySelector('#score');
-  const writer_id = document.querySelector('#writer');
-  const director_id = document.querySelector('#director');
-  const year = document.querySelector('#year');
-  const country = document.querySelector('#country');
-  const studio_id = document.querySelector('#studio');
-  const gross = document.querySelector('#gross');
-  const budget = document.querySelector('#budget');
-  const released = document.querySelector('#released');
-  const votes = document.querySelector('#votes');
-  const runtime = document.querySelector('#runtime');
-  const actor_id = document.querySelector('#actor');
-  const rating = document.querySelector('#rating');
-  list = [name, genre_id, score, writer_id, director_id, year, country, studio_id, gross,
-    budget, released, votes, runtime, actor_id, rating];
-  async function populateForm(list, data) {
-   
-  }
+  const fname = document.querySelector('#title');
+  const fgenre = document.querySelector('#genre');
+  const fscore = document.querySelector('#score');
+  const fwriter = document.querySelector('#writer');
+  const fdirector = document.querySelector('#director');
+  const fyear = document.querySelector('#year');
+  const fcountry = document.querySelector('#country');
+  const fstudio = document.querySelector('#studio');
+  const fgross = document.querySelector('#gross');
+  const fbudget = document.querySelector('#budget');
+  const freleased = document.querySelector('#released');
+  const fvotes = document.querySelector('#votes');
+  const fruntime = document.querySelector('#runtime');
+  const factor = document.querySelector('#actor');
+  const frating = document.querySelector('#rating');
+  list = [
+    fname,
+    fgenre,
+    fscore,
+    fwriter,
+    fdirector,
+    fyear,
+    fcountry,
+    fstudio,
+    fgross,
+    fbudget,
+    freleased,
+    fvotes,
+    fruntime,
+    factor,
+    frating
+  ];
+  async function populateForm(list, ) {}
   async function loadForm(evt) {
     try {
       const response = await fetch(`/api/movies/${id.value}`);
       const json = await response.json();
-      const {data} = json;
+      const { data } = json;
       if (data) {
         lookup.innerHTML = 'Look Up Other';
         editform.classList.remove('hide');
-        await populateForm(list, data);
+        const {
+          filmid,
+          name,
+          director,
+          writer,
+          genre,
+          country,
+          runtime,
+          year,
+          studio,
+          score,
+          votes,
+          budget,
+          fross,
+          released,
+          actor,
+          rating
+        } = { data };
+        await populateForm(list,null,
+          name,
+          director,
+          writer,
+          genre,
+          country,
+          runtime,
+          year,
+          studio,
+          score,
+          votes,
+          budget,
+          fross,
+          released,
+          actor,
+          rating);
       }
       console.log(data);
     } catch (err) {
